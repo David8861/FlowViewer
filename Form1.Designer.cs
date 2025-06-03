02ff6b668a9203a762249ac9f1ee24000e7bc541
Dave <davida8861@gmail.com>
2025-06-03 11:43:05 -0400

    rename menu bar for clarity

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,38 +33,38 @@ namespace FlowViewer
 
         private void InitializeComponent()
         {
-            Forward = new Button();
-            Backward = new Button();
+            DocForward = new Button();
+            DocBackward = new Button();
             DrawingPen = new Button();
             Highlighter = new Button();
-            menuStrip1 = new MenuStrip();
+            topmenu = new MenuStrip();
             toolStripMenuItem1 = new ToolStripMenuItem();
             toolOpen = new ToolStripMenuItem();
+            fileseperate = new ToolStripSeparator();
             toolSave = new ToolStripMenuItem();
-            seperate = new ToolStripSeparator();
             toolStripMenuItem2 = new ToolStripMenuItem();
             toolStripMenuItem3 = new ToolStripMenuItem();
             About = new ToolStripMenuItem();
-            menuStrip1.SuspendLayout();
+            topmenu.SuspendLayout();
             SuspendLayout();
             // 
-            // Forward
+            // DocForward
             // 
-            Forward.Location = new Point(845, 502);
-            Forward.Name = "Forward";
-            Forward.Size = new Size(125, 55);
-            Forward.TabIndex = 0;
-            Forward.Text = "Forward";
-            Forward.UseVisualStyleBackColor = true;
+            DocForward.Location = new Point(845, 502);
+            DocForward.Name = "DocForward";
+            DocForward.Size = new Size(125, 55);
+            DocForward.TabIndex = 0;
+            DocForward.Text = "Forward";
+            DocForward.UseVisualStyleBackColor = true;
             // 
-            // Backward
+            // DocBackward
             // 
-            Backward.Location = new Point(12, 502);
-            Backward.Name = "Backward";
-            Backward.Size = new Size(125, 55);
-            Backward.TabIndex = 0;
-            Backward.Text = "Backward";
-            Backward.UseVisualStyleBackColor = true;
+            DocBackward.Location = new Point(12, 502);
+            DocBackward.Name = "DocBackward";
+            DocBackward.Size = new Size(125, 55);
+            DocBackward.TabIndex = 0;
+            DocBackward.Text = "Backward";
+            DocBackward.UseVisualStyleBackColor = true;
             // 
             // DrawingPen
             // 
@@ -82,19 +82,19 @@ namespace FlowViewer
             Highlighter.TabIndex = 2;
             Highlighter.Text = "Highlight";
             // 
-            // menuStrip1
+            // topmenu
             // 
-            menuStrip1.AllowDrop = true;
-            menuStrip1.Items.AddRange(new ToolStripItem[] { toolStripMenuItem1, toolStripMenuItem2, About });
-            menuStrip1.Location = new Point(0, 0);
-            menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(977, 24);
-            menuStrip1.TabIndex = 3;
-            menuStrip1.Text = "Menu";
+            topmenu.AllowDrop = true;
+            topmenu.Items.AddRange(new ToolStripItem[] { toolStripMenuItem1, toolStripMenuItem2, About });
+            topmenu.Location = new Point(0, 0);
+            topmenu.Name = "menuStrip1";
+            topmenu.Size = new Size(977, 24);
+            topmenu.TabIndex = 3;
+            topmenu.Text = "Menu";
             // 
             // toolStripMenuItem1
             // 
-            toolStripMenuItem1.DropDownItems.AddRange(new ToolStripItem[] { toolOpen, seperate, toolSave });
+            toolStripMenuItem1.DropDownItems.AddRange(new ToolStripItem[] { toolOpen, fileseperate, toolSave });
             toolStripMenuItem1.Name = "toolStripMenuItem1";
             toolStripMenuItem1.Size = new Size(37, 20);
             toolStripMenuItem1.Text = "File";
@@ -105,16 +105,16 @@ namespace FlowViewer
             toolOpen.Size = new Size(103, 22);
             toolOpen.Text = "Open";
             // 
+            // fileseperate
+            // 
+            fileseperate.Name = "fileseperate";
+            fileseperate.Size = new Size(100, 6);
+            // 
             // toolSave
             // 
             toolSave.Name = "toolSave";
             toolSave.Size = new Size(103, 22);
             toolSave.Text = "Save";
-            // 
-            // seperate
-            // 
-            seperate.Name = "seperate";
-            seperate.Size = new Size(100, 6);
             // 
             // toolStripMenuItem2
             // 
@@ -126,7 +126,7 @@ namespace FlowViewer
             // toolStripMenuItem3
             // 
             toolStripMenuItem3.Name = "toolStripMenuItem3";
-            toolStripMenuItem3.Size = new Size(180, 22);
+            toolStripMenuItem3.Size = new Size(127, 22);
             toolStripMenuItem3.Text = "Fullscreen";
             // 
             // About
@@ -140,33 +140,33 @@ namespace FlowViewer
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(977, 569);
-            Controls.Add(Forward);
-            Controls.Add(Backward);
+            Controls.Add(DocForward);
+            Controls.Add(DocBackward);
             Controls.Add(DrawingPen);
             Controls.Add(Highlighter);
-            Controls.Add(menuStrip1);
-            MainMenuStrip = menuStrip1;
+            Controls.Add(topmenu);
+            MainMenuStrip = topmenu;
             Name = "Form1";
             Text = "Flow Viewer";
-            menuStrip1.ResumeLayout(false);
-            menuStrip1.PerformLayout();
+            topmenu.ResumeLayout(false);
+            topmenu.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Button Forward;
-        private Button Backward;
+        private Button DocForward;
+        private Button DocBackward;
         private Button DrawingPen;
         private Button Highlighter;
-        private MenuStrip menuStrip1;
+        private MenuStrip topmenu;
         private ToolStripMenuItem toolStripMenuItem1;
         private ToolStripMenuItem toolStripMenuItem2;
         private ToolStripMenuItem About;
         private ToolStripMenuItem toolOpen;
         private ToolStripMenuItem toolSave;
-        private ToolStripSeparator seperate;
+        private ToolStripSeparator fileseperate;
         private ToolStripMenuItem toolStripMenuItem3;
     }
 }
