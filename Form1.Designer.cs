05f1c5fe000189345c2d83a87fa804a6752f2af8
Dave <davida8861@gmail.com>
2025-06-09 19:21:43 -0400

    comment and organize Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -24,8 +24,6 @@ namespace FlowViewer
             base.Dispose(disposing);
         }
 
-        
-
         /// <summary>
         ///  Required method for Designer support - do not modify
         ///  the contents of this method with the code editor.
@@ -33,24 +31,35 @@ namespace FlowViewer
 
         private void InitializeComponent()
         {
+            // Enables the use of resources
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            
+            // Create a toolbar menu
             topmenu = new MenuStrip();
+
+            // Assign new toolstrip functions for the toolbar
             filemenu = new ToolStripMenuItem();
             toolOpen = new ToolStripMenuItem();
-            fileseperate = new ToolStripSeparator();
             toolSave = new ToolStripMenuItem();
-            fileseperate2 = new ToolStripSeparator();
             toolExit = new ToolStripMenuItem();
             viewmenu = new ToolStripMenuItem();
             bigger = new ToolStripMenuItem();
-            toolStripSeparator1 = new ToolStripSeparator();
             smaller = new ToolStripMenuItem();
-            toolStripSeparator2 = new ToolStripSeparator();
             reset = new ToolStripMenuItem();
             aboutmenu = new ToolStripMenuItem();
-            toolStripSeparator3 = new ToolStripSeparator();
-            menuStrip1 = new MenuStrip();
             toolsmenu = new ToolStripMenuItem();
+
+            // Seperates toolstrip functions from eachother
+            fileseperate = new ToolStripSeparator();
+            fileseperate2 = new ToolStripSeparator();
+            toolStripSeparator1 = new ToolStripSeparator();
+            toolStripSeparator2 = new ToolStripSeparator();
+            toolStripSeparator3 = new ToolStripSeparator();
+
+            // Create menu for Tools
+            menuStrip1 = new MenuStrip();
+
+            // Create tool functions and suspend layout for program use
             drawpen = new ToolStripMenuItem();
             highlight = new ToolStripMenuItem();
             topmenu.SuspendLayout();
@@ -58,7 +67,7 @@ namespace FlowViewer
             SuspendLayout();
             
 
-
+            // Define menu attributes
             topmenu.AllowDrop = true;
             topmenu.BackgroundImage = (Image)resources.GetObject("topmenu.BackgroundImage");
             topmenu.BackgroundImageLayout = ImageLayout.Stretch;
@@ -69,9 +78,8 @@ namespace FlowViewer
             topmenu.Size = new Size(1000, 40);
             topmenu.TabIndex = 3;
             topmenu.Text = "Menu";
-            // 
-            // filemenu
-            // 
+           
+            // Assign file drop down menu attributes for file functions
             filemenu.BackColor = Color.Transparent;
             filemenu.DropDownItems.AddRange(new ToolStripItem[] { toolOpen, fileseperate, toolSave, fileseperate2, toolExit });
             filemenu.Font = new Font("Georgia", 9.75F);
@@ -81,85 +89,73 @@ namespace FlowViewer
             filemenu.Size = new Size(42, 20);
             filemenu.Text = "File";
             filemenu.Click += filemenu_Click;
-            // 
-            // toolOpen
-            // 
+            
+            // Define Open function attributes and mouse click reference
             toolOpen.Image = (Image)resources.GetObject("toolOpen.Image");
             toolOpen.Name = "toolOpen";
             toolOpen.Size = new Size(180, 22);
             toolOpen.Text = "Open";
             toolOpen.Click += toolOpen_Click;
-            // 
-            // fileseperate
-            // 
+            
+            // Seperate menu items
             fileseperate.Name = "fileseperate";
             fileseperate.Size = new Size(177, 6);
-            // 
-            // toolSave
-            // 
+            
+            // Define Save function attributes and mouse click reference
             toolSave.Image = (Image)resources.GetObject("toolSave.Image");
             toolSave.Name = "toolSave";
             toolSave.Size = new Size(180, 22);
             toolSave.Text = "Save";
             toolSave.Click += toolSave_Click;
-            // 
-            // fileseperate2
-            // 
+            
+            // Seperate menu items
             fileseperate2.Name = "fileseperate2";
             fileseperate2.Size = new Size(177, 6);
-            //
-            // toolExit
-            //
+            
+            // Define program exit item and exit on click
             toolExit.Image = (Image)resources.GetObject("toolExit.Image");
             toolExit.Name = "toolExit";
             toolExit.Size = new Size(180, 22);
             toolExit.Text = "Exit";
             toolExit.Click += toolExit_Click;
-            // 
-            // viewmenu
-            // 
+           
+            // Define View menu attributes
             viewmenu.BackColor = Color.White;
             viewmenu.DropDownItems.AddRange(new ToolStripItem[] { bigger, toolStripSeparator1, smaller, toolStripSeparator2, reset });
             viewmenu.Font = new Font("Georgia", 9.75F);
             viewmenu.Name = "viewmenu";
             viewmenu.Size = new Size(50, 20);
             viewmenu.Text = "View";
-            // 
-            // bigger
-            // 
+            
+            // Define enlarge menu 
             bigger.Image = (Image)resources.GetObject("bigger.Image");
             bigger.Name = "bigger";
             bigger.Size = new Size(180, 22);
             bigger.Text = "Bigger";
             bigger.Click += bigger_Click;
-            // 
-            // toolStripSeparator1
-            // 
+            
+            // Seperate menu items
             toolStripSeparator1.Name = "toolStripSeparator1";
             toolStripSeparator1.Size = new Size(177, 6);
-            // 
-            // smaller
-            // 
+            
+            // Define shrink menu 
             smaller.Image = (Image)resources.GetObject("smaller.Image");
             smaller.Name = "smaller";
             smaller.Size = new Size(180, 22);
             smaller.Text = "Smaller";
             smaller.Click += smaller_Click;
-            // 
-            // toolStripSeparator2
-            // 
+            
+            // Seperate menu items
             toolStripSeparator2.Name = "toolStripSeparator2";
             toolStripSeparator2.Size = new Size(177, 6);
-            // 
-            // reset
-            // 
+            
+            // Defines reset back to original window size
             reset.Name = "reset";
             reset.Size = new Size(180, 22);
             reset.Text = "Reset";
             reset.Click += reset_Click;
-            // 
-            // aboutmenu
-            // 
+            
+            // Define about menu attributes
             aboutmenu.Alignment = ToolStripItemAlignment.Right;
             aboutmenu.Font = new Font("Georgia", 9.75F);
             aboutmenu.ForeColor = Color.Black;
@@ -167,14 +163,12 @@ namespace FlowViewer
             aboutmenu.Size = new Size(58, 20);
             aboutmenu.Text = "About";
             aboutmenu.Click += aboutmenu_Click;
-            // 
-            // toolStripSeparator3
-            // 
+            
+            // Seperate menu items
             toolStripSeparator3.Name = "toolStripSeparator3";
             toolStripSeparator3.Size = new Size(141, 6);
-            // 
-            // menuStrip1
-            // 
+            
+            // Creates and defines toolbar dock
             menuStrip1.Dock = DockStyle.Bottom;
             menuStrip1.Items.AddRange(new ToolStripItem[] { toolsmenu });
             menuStrip1.Location = new Point(0, 590);
@@ -183,34 +177,30 @@ namespace FlowViewer
             menuStrip1.Size = new Size(1000, 60);
             menuStrip1.TabIndex = 4;
             menuStrip1.Text = "Dock";
-            // 
-            // toolsmenu
-            // 
+            
+            // Create and assign tools menu attributes
             toolsmenu.DropDownItems.AddRange(new ToolStripItem[] { drawpen, toolStripSeparator3, highlight });
             toolsmenu.Font = new Font("Georgia", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
             toolsmenu.Name = "toolsmenu";
             toolsmenu.Padding = new Padding(0, 15, 0, 15);
             toolsmenu.Size = new Size(45, 50);
             toolsmenu.Text = "Tools";
-            // 
-            // drawpen
-            // 
+            
+            // Assign pen tool attributes
             drawpen.Image = (Image)resources.GetObject("drawpen.Image");
             drawpen.Name = "drawpen";
             drawpen.Size = new Size(144, 22);
             drawpen.Text = "Pen";
             drawpen.Click += drawpen_Click;
-            // 
-            // highlight
-            // 
+            
+            // Assign highlighter tool attributes
             highlight.Image = (Image)resources.GetObject("highlight.Image");
             highlight.Name = "highlight";
             highlight.Size = new Size(144, 22);
             highlight.Text = "Highlighter";
             highlight.Click += highlight_Click;
-            // 
-            // Form1
-            // 
+            
+            // Assign attributes to Flow Viewer and set the tool and menu bars visible 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
@@ -229,25 +219,13 @@ namespace FlowViewer
             PerformLayout();
         }
 
-        private MenuStrip topmenu;
-        private ToolStripMenuItem filemenu;
-        private ToolStripMenuItem viewmenu;
-        private ToolStripMenuItem aboutmenu;
-        private ToolStripMenuItem toolExit;
-        private ToolStripMenuItem toolOpen;
-        private ToolStripMenuItem toolSave;
-        private ToolStripSeparator fileseperate;
-        private ToolStripSeparator fileseperate2;
-        private ToolStripMenuItem bigger;
-        private ToolStripMenuItem smaller;
-        private ToolStripMenuItem reset;
-        private ToolStripSeparator toolStripSeparator1;
-        private ToolStripSeparator toolStripSeparator2;
-        private ToolStripSeparator toolStripSeparator3;
-        private MenuStrip menuStrip1;
-        private ToolStripMenuItem toolsmenu;
-        private ToolStripMenuItem highlight;
-        private ToolStripMenuItem drawpen;
-        
+        // Create menu, item and seperator classes for Flow Viewer
+        private MenuStrip topmenu, menuStrip1;
+
+        private ToolStripMenuItem filemenu, viewmenu, aboutmenu, toolOpen, toolSave, toolExit;
+        private ToolStripMenuItem bigger, smaller, reset, toolsmenu, drawpen, highlight;
+
+        private ToolStripSeparator fileseperate, fileseperate2, toolStripSeparator1, toolStripSeparator2, toolStripSeparator3;
+      
     }
 }
